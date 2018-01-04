9f47dd1dde079d6ca10437531790c53a6671c0b8
Matthew DeJonge <mhdejong@umich.edu>
2018-01-04 17:39:42 -0500

    Dependencies to dev dependencies and vscode tasks.

--- CFlat/test/tests.ts
+++ CFlat/test/tests.ts
@@ -1,8 +1,8 @@
-import { expect } from "chai";
-import "../src/index.cb.js"
+import { expect } from 'chai';
+import '../src/index.cb.js'
 
-describe("string.isBlank", () => {
-    it("empty string, whitespace, and string with text", () => {
+describe('string.isBlank', () => {
+    it('empty string, whitespace, and string with text', () => {
         expect(''.isBlank()).is.true;
         expect('  '.isBlank()).is.true;
         expect('NotBlank'.isBlank()).is.false;
@@ -11,14 +11,14 @@ describe("string.isBlank", () => {
 
 const array = [1, 2, 3];
 
-describe("array.first", () => {
-    it("get first value of array", () => {
+describe('array.first', () => {
+    it('get first value of array', () => {
         expect(array.first()).to.eq(1);
     });
 });
 
-describe("array.last", () => {
-    it("get last value of array", () => {
+describe('array.last', () => {
+    it('get last value of array', () => {
         expect(array.last()).to.eq(3);
     });
 });
