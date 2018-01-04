de45cea96a46bc76a8e6c6ad784932abf6a40b8e
Matthew DeJonge <mhdejong@umich.edu>
2018-01-04 11:59:50 -0500

    Cleanup

--- CFlat/test/tests.ts
+++ CFlat/test/tests.ts
@@ -13,12 +13,12 @@ const array = [1, 2, 3];
 
 describe("array.first", () => {
     it("get first value of array", () => {
-        expect(array.first()).to.eq(1)
+        expect(array.first()).to.eq(1);
     });
 });
 
 describe("array.last", () => {
     it("get last value of array", () => {
-        expect(array.last()).to.eq(3)
+        expect(array.last()).to.eq(3);
     });
 });
